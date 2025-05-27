77924c40b8cbd14fd9e2d8a4556ebaf70f90f94d
offsec1 <offsec1@protonmail.com>
2025-05-27 08:08:37 +0200

    create the git-artisan bash file and execute a test command

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 )
+
+const artisanFile = "/tmp/git-artisan.sh"
 
 var year int
 var githubProject string
@@ -26,4 +30,40 @@ func main() {
 	fmt.Printf("The pixel art will be printed in following year: %d\n", year)
 	fmt.Printf("For the dummy commits following project will be used: %s\n", githubProject)
 
+	fmt.Println("Starting the artisan...")
+
+	f, err := os.Create(artisanFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = f.WriteString("echo 'Hello World'")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	fmt.Println("Successfully created /tmp/git-artisan.sh")
+
+	out, err := exec.Command("bash", artisanFile).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Got following output from command execution: %s\n", out)
+
+	defer func() {
+		err := os.Remove(artisanFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	fmt.Println("The artisan is done.")
 }
